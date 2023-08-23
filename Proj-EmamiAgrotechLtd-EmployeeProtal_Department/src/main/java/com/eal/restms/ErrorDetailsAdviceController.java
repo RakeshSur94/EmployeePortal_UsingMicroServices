fe8454f99f34c2605ca9785541fc776b09c4a9b4
Rakesh Sur <rakeshsur72@gmail.com>
2023-08-23 11:46:43 +0530

    intial commit

--- Proj-EmamiAgrotechLtd-EmployeeProtal_Department/src/main/java/com/eal/restms/ErrorDetailsAdviceController.java
+++ Proj-EmamiAgrotechLtd-EmployeeProtal_Department/src/main/java/com/eal/restms/ErrorDetailsAdviceController.java
@@ -3,6 +3,7 @@ package com.eal.restms;
 import java.time.LocalDateTime;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -15,7 +16,7 @@ public class ErrorDetailsAdviceController {
 	@ExceptionHandler(CompanyNotFoundException.class)
 	public ResponseEntity<ErrorDetails> comanyNotFoundException(CompanyNotFoundException cnfe){
 		ErrorDetails details=new ErrorDetails(LocalDateTime.now(), cnfe.getMessage(), "404-Request resourse not found");
-		return new ResponseEntity<ErrorDetails>(details, HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<ErrorDetails>(details, HttpStatus.NOT_FOUND);
 	}
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorDetails> allException(Exception e){
